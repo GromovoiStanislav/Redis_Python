94482a737f470bbd281061a994a80abdd9767014
Gromovoi Stanislav <gromila12@rambler.ru>
2023-10-03 09:56:18 +0600

    Project 2

--- Project 2/main.py
+++ Project 2/main.py
@@ -1,10 +1,17 @@
 import asyncio
+import os
+
 import aioredis
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 async def main1():
-    print('************** Strings *****************')
     redis = aioredis.from_url("redis://localhost", db=0)
+
+    print('************** Strings *****************')
+
     await redis.set("key", "string-value")
     bin_value = await redis.get("key")
     assert bin_value == b"string-value"
@@ -63,6 +70,7 @@ async def main1():
             print('"ToDelete1" not found...')
 
     print('************** incr *****************')
+
     await redis.set("counter", 0)
     await redis.incr("counter")
     print(await redis.get("counter"))  # 1
@@ -86,9 +94,11 @@ async def main1():
 
 
 async def main2():
-    print('************** Hash *****************')
     redis = aioredis.from_url("redis://localhost", db=0, decode_responses=True)
-    await redis.hmset("hash", mapping={"key1": "value1", "key2": "value2", "key3": "123"})
+
+    print('************** Hash *****************')
+
+    await redis.hmset("hash", mapping={"key1": "value1", "key2": "value2", "key3": 123})
     result = await redis.hgetall("hash")
 
     assert result == {
@@ -119,9 +129,11 @@ async def main2():
 
 
 async def main3():
-    # Transactions
-    print('************** Pipeline *****************')
+
     redis = await aioredis.from_url("redis://localhost", db=0, decode_responses=True)
+
+    print('************** Pipeline *****************') # Transactions
+
     async with redis.pipeline(transaction=True) as pipe:
         ok1, ok2 = await (pipe.set("key1", "value1").set("key2", "value2").execute())
     assert ok1
@@ -138,28 +150,73 @@ async def main3():
 
 
 async def main4():
+    REDIS_URL = os.environ.get('REDIS_URL')
+
     # List
-    async with aioredis.from_url("redis://localhost", db=0, decode_responses=True) as redis:
+    async with aioredis.from_url(REDIS_URL, db=0, decode_responses=True) as redis:
+
         print('************** List *****************')
+
         if await redis.exists("my-list"):
             await redis.delete('my-list')
+
+        if await redis.exists("new-list"):
+            await redis.delete('new-list')
 
         await redis.rpush('my-list', 'a', 'b', 'c')
         await redis.rpush('my-list', 'd')
 
         print(await redis.lrange('my-list', 0, -1))  # ['a', 'b', 'c', 'd']
         print(await redis.llen('my-list'))  # 4
-        # print(await redis.lpos('my-list', 'b'))  # 1
-        # print(await redis.lpos('my-list', 'G'))  # None
+        print(await redis.lpos('my-list', 'b'))  # 1
+        print(await redis.lpos('my-list', 'G'))  # None
         print(await redis.lindex('my-list', 10))  # None
 
+        await redis.linsert('my-list', 'BEFORE', 'b', 'BB')
+        await redis.linsert('my-list', 'AFTER', 'b', 'CC')
+        await redis.lset('my-list', 0, 'AA')
+        print(await redis.lrange('my-list', 0, -1))  # ['AA', 'BB', 'b', 'CC', 'c', 'd']
 
+        await redis.lrem('my-list', 0, 'BB')
+        print(await redis.lrange('my-list', 0, -1))  # ['AA', 'b', 'CC', 'c', 'd']
+
+        print(await redis.lpop('my-list'))  # AA
+        print(await redis.rpop('my-list'))  # d
+        print(await redis.lrange('my-list', 0, -1))  # ['b', 'CC', 'c']
+
+        print(await redis.rpoplpush('my-list', 'my-list'))  # c
+        print(await redis.lrange('my-list', 0, -1))  # ['c', 'b', 'CC']
+
+        while await redis.llen('my-list'):
+            await redis.rpoplpush('my-list', 'new-list')
+
+        print(await redis.lrange('my-list', 0, -1))  # []
+        print(await redis.lrange('new-list', 0, -1))  # ['c', 'b', 'CC']
+
+        print(await redis.execute_command("lMOVE", 'new-list', 'my-list', 'LEFT', 'RIGHT'))  # c
+        print(await redis.execute_command("lMOVE", 'new-list', 'my-list', 'LEFT', 'RIGHT'))  # b
+        print(await redis.execute_command("lMOVE", 'new-list', 'my-list', 'LEFT', 'LEFT'))  # CC
+        print(await redis.lrange('my-list', 0, -1))  # ['CC', 'c', 'b']
+        print(await redis.lrange('new-list', 0, -1))  # []
+
+        # Remove key in 10 sec
+        await redis.expire('my-list', 10)
+        print(await redis.ttl('my-list'))  # 10
+
+        # delete now
+        await redis.delete('new-list')
+        if await redis.exists('new-list'):
+            print('new-list exists')
+        else:
+            print('Not found...')  # Not found...
 
 
 async def main_pool():
     print('************** Pool *****************')
+
     pool = aioredis.ConnectionPool.from_url("redis://localhost", max_connections=10, decode_responses=True)
     redis = aioredis.Redis(connection_pool=pool)
+    
     await redis.execute_command("set", "my-key", "my-value")
     val = await redis.execute_command("get", "my-key")
     print("raw value:", val)
