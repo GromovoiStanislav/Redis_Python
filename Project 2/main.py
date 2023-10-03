ed8bb5780726b15a1143f411679341fa1eec4811
Gromovoi Stanislav <gromila12@rambler.ru>
2023-10-03 12:42:54 +0600

    Project 2

--- Project 2/main.py
+++ Project 2/main.py
@@ -129,10 +129,9 @@ async def main2():
 
 
 async def main3():
-
     redis = await aioredis.from_url("redis://localhost", db=0, decode_responses=True)
 
-    print('************** Pipeline *****************') # Transactions
+    print('************** Pipeline *****************')  # Transactions
 
     async with redis.pipeline(transaction=True) as pipe:
         ok1, ok2 = await (pipe.set("key1", "value1").set("key2", "value2").execute())
@@ -211,20 +210,158 @@ async def main4():
             print('Not found...')  # Not found...
 
 
+async def main5():
+    import json
+    REDIS_URL = os.environ.get('REDIS_URL')
+
+    # List
+    async with aioredis.from_url(REDIS_URL, db=0, decode_responses=True) as redis:
+
+        print('************** JSON *****************')
+
+        if await redis.exists("doc"):
+            await redis.delete('doc')
+
+        my_dict = {"key1": "value1", "key2": "value2", "key3": 123, "key4": {"a": 2}}
+        json_str = json.dumps(my_dict)
+
+        await redis.execute_command('JSON.SET', "doc", '$', json_str)
+        print(await redis.execute_command('JSON.GET',
+                                          "doc"))  # {"key1":"value1","key2":"value2","key3":123,"key4":{"a":2}} <class 'str'>
+
+        print(await redis.execute_command('JSON.OBJKEYS', "doc"))  # ['key1', 'key2', 'key3', 'key4']
+        print(await redis.execute_command('JSON.OBJKEYS', "doc", 'key4'))  # ['a']
+
+        print(await redis.execute_command('JSON.OBJLEN', "doc"))  # 4
+        print(await redis.execute_command('JSON.OBJLEN', "doc", 'key4'))  # 1
+
+        await redis.execute_command('JSON.SET', "doc", 'key3', 10)
+        await redis.execute_command('JSON.SET', "doc", 'key4.a', 3)
+
+        await redis.execute_command('JSON.SET', "doc", 'key5', '"value5"')
+        await redis.execute_command('JSON.SET', "doc", 'key4.b', 4)
+
+        print(await redis.execute_command('JSON.GET',
+                                          "doc"))  # {"key1":"value1","key2":"value2","key3":10,"key4":{"a":3,"b":4}},"key5":"value5"}
+
+        print(await redis.execute_command('JSON.GET', "doc", '$.key3'))  # [10] <class 'str'>
+        print(await redis.execute_command('JSON.GET', "doc", 'key3'))  # 10 <class 'str'>
+        print(await redis.execute_command('JSON.GET', "doc", '$.key4'))  # [{"a":3,"b":4}] <class 'str'>
+        print(await redis.execute_command('JSON.GET', "doc", 'key4'))  # {"a":3,"b":4} <class 'str'>
+        print(await redis.execute_command('JSON.GET', "doc", '$.key4.a'))  # [3] <class 'str'>
+        print(await redis.execute_command('JSON.GET', "doc", 'key4.a'))  # 3  <class 'str'>
+
+        await redis.execute_command('JSON.NUMMULTBY ', "doc", 'key3', 2)
+        print(await redis.execute_command('JSON.GET', "doc", 'key3'))  # 20
+
+        await redis.execute_command('JSON.NUMINCRBY  ', "doc", 'key3', 4)
+        print(await redis.execute_command('JSON.GET', "doc", 'key3'))  # 24
+
+        await redis.execute_command('JSON.NUMINCRBY  ', "doc", 'key4.a', 4)
+        print(await redis.execute_command('JSON.GET', "doc", 'key4.a'))  # 7
+
+        await redis.execute_command('JSON.DEL', "doc", 'key4.a')
+        print(await redis.execute_command('JSON.GET',
+                                          "doc"))  # {"key1":"value1","key2":"value2","key3":24,"key4":{"b":4},"key5":"value5"}
+
+        await redis.execute_command('JSON.DEL', "doc", 'key4')
+        print(await redis.execute_command('JSON.GET',
+                                          "doc"))  # {"key1":"value1","key2":"value2","key3":24,"key5":"value5"}
+
+        await redis.execute_command('JSON.CLEAR ', "doc")
+        print(await redis.execute_command('JSON.GET', "doc"))  # {}
+
+        # ----------------------------------
+
+        my_dict = {"nums": [1, 2, 3], "colors": ["black", "white"], "max_level": [80, 100, 120]}
+        json_str = json.dumps(my_dict)
+
+        await redis.execute_command('JSON.SET', "doc", '$', json_str)
+        print(await redis.execute_command('JSON.GET', "doc"))  # {"arr":[1,2,3]} <class 'str'>
+
+        print(await redis.execute_command('JSON.ARRLEN ', "doc", "$.max_level"))  # [3]  <class 'str'>
+        print(await redis.execute_command('JSON.ARRLEN ', "doc", "max_level"))  # 3  <class 'str'>
+
+        await redis.execute_command('JSON.ARRAPPEND ', "doc", 'colors ', '"blue"')
+        await redis.execute_command('JSON.ARRAPPEND ', "doc", 'nums ', 4, 5, 6)
+        print(await redis.execute_command('JSON.GET',
+                                          "doc"))  # {"nums":[1,2,3,4,5,6],"colors":["black","white","blue"],"max_level":[80,100,120]}
+
+        print(await redis.execute_command('JSON.GET', "doc", '$.colors'))  # [["black","white","blue"]]
+        print(await redis.execute_command('JSON.GET', "doc", '$.colors[*]'))  # ["black","white","blue"]
+        print(await redis.execute_command('JSON.GET', "doc", 'colors'))  # ["black","white","blue"]
+        print(await redis.execute_command('JSON.GET', "doc", 'colors[0]'))  # "black"
+        print(await redis.execute_command('JSON.GET', "doc", 'colors[1]'))  # "white"
+        print(await redis.execute_command('JSON.GET', "doc", 'colors[2]'))  # "white"
+        print(await redis.execute_command('JSON.GET', "doc", 'colors[-1]'))  # "blue"
+        print(await redis.execute_command('JSON.GET', "doc", 'colors[-2]'))  # "white"
+
+        print(await redis.execute_command('JSON.ARRINDEX', "doc", '$.colors', '"blue"'))  # [2]
+        print(await redis.execute_command('JSON.ARRINDEX', "doc", 'colors', '"blue"'))  # 2
+
+        print(await redis.execute_command('JSON.GET', "doc", 'colors'))  # ["black","white","blue"]
+        await redis.execute_command('JSON.ARRINSERT  ', "doc", 'colors ', 2, '"yellow"', '"gold"')
+        print(await redis.execute_command('JSON.GET', "doc", 'colors'))  # ["black","white","yellow","gold","blue"]
+
+        print(await redis.execute_command('JSON.ARRPOP', "doc", 'nums'))  # 6
+        print(await redis.execute_command('JSON.GET', "doc", 'nums'))  # [1.2,3,4,5]
+        print(await redis.execute_command('JSON.ARRPOP', "doc", 'nums', 0))  # 1
+        print(await redis.execute_command('JSON.GET', "doc", 'nums'))  # [2,3,4,5]
+        print(await redis.execute_command('JSON.ARRPOP', "doc", 'nums', 2))  # 4
+        print(await redis.execute_command('JSON.GET', "doc", 'nums'))  # [2,3,5]
+
+        print(await redis.execute_command('JSON.GET', "doc", 'colors'))  # ["black","white","yellow","gold","blue"]
+        await redis.execute_command('JSON.ARRTRIM ', "doc", 'colors', 1, 2)
+        print(await redis.execute_command('JSON.GET', "doc", 'colors'))  # ["white","yellow"]
+
+        print(await redis.execute_command('JSON.GET', "doc", 'max_level'))  # [80, 100, 120]
+        await redis.execute_command('JSON.NUMINCRBY  ', "doc", 'max_level[0]', 4)
+        await redis.execute_command('JSON.NUMINCRBY  ', "doc", 'max_level[-1]', -20)
+        await redis.execute_command('JSON.NUMMULTBY   ', "doc", 'max_level[1]', 2)
+        print(await redis.execute_command('JSON.GET', "doc", 'max_level'))  # [84, 200, 100]
+
+        json_str = await redis.execute_command('JSON.GET', "doc")
+        my_dict = json.loads(json_str)
+        print(my_dict)
+
+        # ----------------------------------
+        await redis.execute_command('JSON.SET', "doc1", '$', json.dumps({"a": 1, "b": 2, "nested": {"a": 3, "b": 2}}))
+        await redis.execute_command('JSON.SET', "doc2", '$', json.dumps({"a": 4, "b": 5, "nested": {"a": 6, "b": 5}}))
+
+        print(await redis.execute_command('JSON.MGET ', "doc1", "doc2", 'nested'))  # ['{"a":3,"b":2}', '{"a":6,"b":5}']
+        print(await redis.execute_command('JSON.MGET ', "doc1", "doc2", 'nested.a'))  # ['3', '6']
+        print(await redis.execute_command('JSON.MGET ', "doc1", "doc2", 'b'))  # ['2', '5']
+
+        # ----------------------------------
+
+        # Remove key in 10 sec
+        await redis.expire('doc1', 10)
+        await redis.expire('doc2', 10)
+        print(await redis.ttl('doc1'))  # 10
+
+        # delete now
+        await redis.delete('doc')
+        if await redis.exists('doc'):
+            print('doc exists')
+        else:
+            print('Not found...')  # Not found...
+
+
 async def main_pool():
     print('************** Pool *****************')
 
     pool = aioredis.ConnectionPool.from_url("redis://localhost", max_connections=10, decode_responses=True)
     redis = aioredis.Redis(connection_pool=pool)
-    
+
     await redis.execute_command("set", "my-key", "my-value")
     val = await redis.execute_command("get", "my-key")
     print("raw value:", val)
 
 
 if __name__ == '__main__':
-    asyncio.run(main1())
-    asyncio.run(main2())
-    asyncio.run(main3())
-    asyncio.run(main4())
-    asyncio.run(main_pool())
+    # asyncio.run(main1())
+    # asyncio.run(main2())
+    # asyncio.run(main3())
+    # asyncio.run(main4())
+    asyncio.run(main5())
+    # asyncio.run(main_pool())
